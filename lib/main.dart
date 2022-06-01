d631e2b68ead940551d3a6181809bd52f3b496f7
Mohamed Ahmed <mohamed.ahmed@8worx.com>
2022-06-01 23:48:20 +0200

    change diemntion to be dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:animated_switch_grid_list/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,50 +41,57 @@ class Option1 extends StatefulWidget {
 }
 
 class _Option1State extends State<Option1> with TickerProviderStateMixin {
-  late Animation<double>? animationAfterIndex1YAxis;
-  late Animation<double>? animationAfterindex1XAxis;
-  late Animation<double>? animationIndexEventYAxis;
-  late Animation<double>? animationTextPostitionLeftSideXAxis;
-  late Animation<double>? animationTextPostitionRightSideYAxis;
-  late Animation<Color?> animateTextColor;
-  late AnimationController? animationController;
+  Animation<double>? animationAfterIndex1YAxis;
+  Animation<double>? animationAfterindex1XAxis;
+  Animation<double>? animationIndexEventYAxis;
+  Animation<double>? animationTextPostitionLeftSideXAxis;
+  Animation<double>? animationTextPostitionRightSideYAxis;
+  Animation<Color?>? animateTextColor;
+  AnimationController? animationController;
   final int multiplyIn = 2;
-  var screenSize ;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
+  Size? screenSize;
+  bool isList = false;
+  bool isScreenLoaded = false;
+  _myFunction(context) {
+    screenSize = MediaQuery.of(context).size;
+    isScreenLoaded = false;
     animationController = AnimationController(
       vsync: this,
       duration: Duration(seconds: 3),
     );
-    animationAfterIndex1YAxis = Tween<double>(begin: 0, end: 210).animate(
+    animationAfterIndex1YAxis = Tween<double>(begin: 0, end: 80).animate(
+        // y=210
         CurvedAnimation(
             parent: animationController!, curve: Curves.easeInOutCubic))
       ..addListener(() {
         setState(() {});
       });
-    animationAfterindex1XAxis = Tween<double>(begin: 0, end: -207).animate(
-        CurvedAnimation(
-            parent: animationController!, curve: Curves.easeInOutCubic))
-      ..addListener(() {
-        setState(() {});
-      });
+    animationAfterindex1XAxis =
+        Tween<double>(begin: 0, end: -screenSize!.width / 2 + 6).animate(
+            // x = -207
+            CurvedAnimation(
+                parent: animationController!, curve: Curves.easeInOutCubic))
+          ..addListener(() {
+            setState(() {});
+          });
 
-    animationIndexEventYAxis = Tween<double>(begin: 0, end: 210).animate(
+    animationIndexEventYAxis = Tween<double>(begin: 0, end: 80).animate(
+        // y = 210
         CurvedAnimation(
             parent: animationController!, curve: Curves.easeInOutCubic))
       ..addListener(() {
         setState(() {});
       });
-    animationTextPostitionLeftSideXAxis = Tween<double>(begin: 0, end: 210)
+    animationTextPostitionLeftSideXAxis = Tween<double>(
+            begin: 0, end: screenSize!.width / 21) // left to right by 210
         .animate(CurvedAnimation(
             parent: animationController!, curve: Curves.easeInOutCubic))
       ..addListener(() {
         setState(() {});
       });
 
-    animationTextPostitionRightSideYAxis = Tween<double>(begin: 0, end: 210)
+    animationTextPostitionRightSideYAxis = Tween<double>(
+            begin: 0, end: screenSize!.height * 80) //top to bottom by 210
         .animate(CurvedAnimation(
             parent: animationController!, curve: Curves.easeInOutCubic))
       ..addListener(() {
@@ -92,11 +100,20 @@ class _Option1State extends State<Option1> with TickerProviderStateMixin {
     animateTextColor = ColorTween(begin: Colors.white, end: Colors.black)
         .animate(CurvedAnimation(
             parent: animationController!, curve: Curves.easeInOutCubic));
+    setState(() {
+      isScreenLoaded = true;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => _myFunction(context));
   }
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Column(
         children: [
@@ -106,6 +123,8 @@ class _Option1State extends State<Option1> with TickerProviderStateMixin {
           Align(
             child: RaisedButton(
               onPressed: () {
+                isList = !isList;
+                setState(() {});
                 if (!animationController!.isCompleted)
                   animationController!.forward();
                 else
@@ -118,44 +137,59 @@ class _Option1State extends State<Option1> with TickerProviderStateMixin {
                   borderRadius: BorderRadius.all(Radius.circular(20))),
             ),
           ),
-          Expanded(
-            child: GridView.builder(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: animationController!.isCompleted ? 1 : 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10.0,
-                mainAxisExtent: 200,
-              ),
-              itemCount: 6,
-              itemBuilder: (context, index) => Transform.translate(
-                offset: animationController!.isCompleted
-                    ? const Offset(0, 0)
-                    : index != 0
-                        ? index.isOdd
-                            ? index > 1
-                                ? Offset(animationAfterindex1XAxis!.value,
-                                    animationAfterIndex1YAxis!.value * 2)
-                                : Offset(animationAfterindex1XAxis!.value,
-                                    animationAfterIndex1YAxis!.value)
-                            : Offset(0, animationIndexEventYAxis!.value)
-                        : const Offset(0, 0),
-                child: animationController!.isCompleted
-                    ? Row(
-                        children: [
-                          SizedBox(
-                            width: 200,
-                            height: 200,
-                            child: buildCard(index),
-                          ),
-                        ],
-                      )
-                    : buildCard(index),
-              ),
-            ),
-          ),
+          isScreenLoaded == false
+              ? Container()
+              : Expanded(
+                  child: GridView.builder(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 20.0),
+                    shrinkWrap: true,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: animationController!.isCompleted ? 1 : 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10.0,
+                      mainAxisExtent: 70,
+                    ),
+                    itemCount: 20,
+                    itemBuilder: (context, index) => Transform.translate(
+                      offset: animationController!.isCompleted
+                          ? const Offset(0, 0)
+                          : index != 0
+                              ? index.isOdd
+                                  ? index > 1
+                                      ? Offset(
+                                          animationAfterindex1XAxis!.value,
+                                          animationAfterIndex1YAxis!.value *
+                                              (index - 1) ) // every
+                                      : Offset(animationAfterindex1XAxis!.value,
+                                          animationAfterIndex1YAxis!.value)
+                                  : index > 2
+                                      ? Offset(
+                                          0,
+                                          animationIndexEventYAxis!.value *
+                                              (index / 2))
+                                      : Offset(
+                                          0, animationIndexEventYAxis!.value)
+                              : const Offset(0, 0),
+                      child: animationController!.isCompleted
+                          ? Row(
+                              children: [
+                                Expanded(
+                                    child: Container(
+                                        margin: EdgeInsets.only(right: 5),
+                                        child: buildCard(index))),
+                                Expanded(
+                                  child: Visibility(
+                                    visible: false,
+                                    child: Container(),
+                                  ),
+                                ),
+                              ],
+                            )
+                          : buildCard(index),
+                    ),
+                  ),
+                ),
         ],
       ),
     );
@@ -168,16 +202,21 @@ class _Option1State extends State<Option1> with TickerProviderStateMixin {
   }
 
   Widget buildCard(index) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Colors.red,
-      ),
-      child: Transform.translate(
-        offset: Offset(animationTextPostitionLeftSideXAxis!.value, 0),
-        child: Text(
-          "Yalla Bena Now $index",
-          style: TextStyle(
-            color: animateTextColor.value,
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: 80,
+        decoration: const BoxDecoration(
+          color: Colors.red,
+        ),
+        child: Transform.translate(
+          offset: Offset(animationTextPostitionLeftSideXAxis!.value, 0),
+          child: Text(
+            "Yalla Bena Now $index",
+            style: TextStyle(
+              fontSize: 25,
+              color: animateTextColor!.value,
+            ),
           ),
         ),
       ),
