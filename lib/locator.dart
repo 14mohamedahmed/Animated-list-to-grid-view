10a5c36a8614e564a299f85b42f56409478bb11e
Mohamed Ahmed <mohamed.ahmed@8worx.com>
2022-06-13 21:35:48 +0200

    Enhance Animation Coding & Finished

--- lib/locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:animated_switch_grid_list/screen_sizes.dart';
-import 'package:get_it/get_it.dart';
-
-GetIt locator = GetIt.I;
-
-setupLocators(){
-  locator.registerLazySingleton(() => ScreenSizes());
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:animated_switch_grid_list/home/home.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/scheduler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,217 +6,205 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  static int view = 1;
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Switch Animation',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        brightness: Brightness.light,
       ),
-      home: Option1(context: context),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
 
-class Option1 extends StatefulWidget {
-  final BuildContext context;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-  const Option1({super.key, required this.context});
   @override
-  _Option1State createState() => _Option1State();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _Option1State extends State<Option1> with TickerProviderStateMixin {
-  Animation<double>? animationAfterIndex1YAxis;
-  Animation<double>? animationAfterindex1XAxis;
-  Animation<double>? animationIndexEventYAxis;
-  Animation<double>? animationTextPostitionLeftSideXAxis;
-  Animation<double>? animationTextPostitionRightSideYAxis;
-  Animation<Color?>? animateTextColor;
-  AnimationController? animationController;
-  final int multiplyIn = 2;
-  Size? screenSize;
-  bool isList = false;
-  bool isScreenLoaded = false;
-  _myFunction(context) {
-    screenSize = MediaQuery.of(context).size;
-    isScreenLoaded = false;
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 3),
-    );
-    animationAfterIndex1YAxis = Tween<double>(begin: 0, end: 80).animate(
-        // y=210
-        CurvedAnimation(
-            parent: animationController!, curve: Curves.easeInOutCubic))
-      ..addListener(() {
-        setState(() {});
-      });
-    animationAfterindex1XAxis =
-        Tween<double>(begin: 0, end: -screenSize!.width / 2 + 6).animate(
-            // x = -207
-            CurvedAnimation(
-                parent: animationController!, curve: Curves.easeInOutCubic))
-          ..addListener(() {
-            setState(() {});
-          });
-
-    animationIndexEventYAxis = Tween<double>(begin: 0, end: 80).animate(
-        // y = 210
-        CurvedAnimation(
-            parent: animationController!, curve: Curves.easeInOutCubic))
-      ..addListener(() {
-        setState(() {});
-      });
-    animationTextPostitionLeftSideXAxis = Tween<double>(
-            begin: 0, end: screenSize!.width / 21) // left to right by 210
-        .animate(CurvedAnimation(
-            parent: animationController!, curve: Curves.easeInOutCubic))
-      ..addListener(() {
-        setState(() {});
-      });
-
-    animationTextPostitionRightSideYAxis = Tween<double>(
-            begin: 0, end: screenSize!.height * 80) //top to bottom by 210
-        .animate(CurvedAnimation(
-            parent: animationController!, curve: Curves.easeInOutCubic))
-      ..addListener(() {
-        setState(() {});
-      });
-    animateTextColor = ColorTween(begin: Colors.white, end: Colors.black)
-        .animate(CurvedAnimation(
-            parent: animationController!, curve: Curves.easeInOutCubic));
-    setState(() {
-      isScreenLoaded = true;
-    });
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) => _myFunction(context));
-  }
+class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('GridView - ListView Switcher'),
+        elevation: 0.0,
+      ),
       body: Column(
         children: [
-          SizedBox(
-            height: 50.0,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  MyApp.view = 2;
+                  Navigator.of(context)
+                      .pushReplacement(_createRoute(const MyHomePage()));
+                },
+                icon: const Icon(
+                  Icons.list,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(width: 25),
+              IconButton(
+                onPressed: () {
+                  MyApp.view = 1;
+                  Navigator.of(context)
+                      .pushReplacement(_createRoute(const MyHomePage()));
+                },
+                icon: const Icon(
+                  Icons.grid_view_outlined,
+                  color: Colors.black,
+                ),
+              ),
+            ],
           ),
-          Align(
-            child: RaisedButton(
-              onPressed: () {
-                isList = !isList;
-                setState(() {});
-                if (!animationController!.isCompleted)
-                  animationController!.forward();
-                else
-                  animationController!.reverse();
-              },
-              child: Text('Go'),
-              color: Colors.red,
-              textColor: Colors.yellowAccent,
-              shape: BeveledRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-            ),
-          ),
-          isScreenLoaded == false
-              ? Container()
-              : Expanded(
-                  child: GridView.builder(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 10.0, vertical: 20.0),
-                    shrinkWrap: true,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: animationController!.isCompleted ? 1 : 2,
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10.0,
-                      mainAxisExtent: 70,
+          Expanded(
+            child: (MyApp.view == 2)
+                ? ListView.builder(
+                    itemCount: 12,
+                    itemBuilder: (context, index) => Hero(
+                      tag: index,
+                      child: Card(
+                        child: Container(
+                          height: 290,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20)),
+                          margin: const EdgeInsets.all(5),
+                          padding: const EdgeInsets.all(5),
+                          child: Stack(
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Expanded(
+                                    child: Image.asset(
+                                      'assets/img-1.jpg',
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                  const Text(
+                                    'Title',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  const Text(
+                                    'Subtitle',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                      color: Colors.black,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                    itemCount: 20,
-                    itemBuilder: (context, index) => Transform.translate(
-                      offset: animationController!.isCompleted
-                          ? const Offset(0, 0)
-                          : index != 0
-                              ? index.isOdd
-                                  ? index > 1
-                                      ? Offset(
-                                          animationAfterindex1XAxis!.value,
-                                          animationAfterIndex1YAxis!.value *
-                                              (index - 1) ) // every
-                                      : Offset(animationAfterindex1XAxis!.value,
-                                          animationAfterIndex1YAxis!.value)
-                                  : index > 2
-                                      ? Offset(
-                                          0,
-                                          animationIndexEventYAxis!.value *
-                                              (index / 2))
-                                      : Offset(
-                                          0, animationIndexEventYAxis!.value)
-                              : const Offset(0, 0),
-                      child: animationController!.isCompleted
-                          ? Row(
-                              children: [
-                                Expanded(
-                                    child: Container(
-                                        margin: EdgeInsets.only(right: 5),
-                                        child: buildCard(index))),
-                                Expanded(
-                                  child: Visibility(
-                                    visible: false,
-                                    child: Container(),
+                  )
+                : GridView.builder(
+                    itemCount: 12,
+                    physics: const PageScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                    padding: const EdgeInsets.all(8.0),
+                    itemBuilder: (context, index) => Hero(
+                      tag: index,
+                      child: Card(
+                        child: Container(
+                          height: 290,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20)),
+                          margin: const EdgeInsets.all(5),
+                          padding: const EdgeInsets.all(5),
+                          alignment: Alignment.center,
+                          child: Stack(
+                            children: [
+                              Image.asset(
+                                'assets/img.jpg',
+                                fit: BoxFit.fill,
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                          colors: [
+                                        Colors.black.withOpacity(0.7),
+                                        Colors.black.withOpacity(0.3)
+                                      ],
+                                          begin: Alignment.bottomCenter,
+                                          end: Alignment.topCenter)),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: const [
+                                      Text(
+                                        'Title',
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Subtitle',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15,
+                                          color: Colors.white,
+                                        ),
+                                      )
+                                    ],
                                   ),
                                 ),
-                              ],
-                            )
-                          : buildCard(index),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+          ),
         ],
       ),
     );
   }
+}
 
-  @override
-  void dispose() {
-    animationController!.dispose();
-    super.dispose();
-  }
-
-  Widget buildCard(index) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        height: 80,
-        decoration: const BoxDecoration(
-          color: Colors.red,
-        ),
-        child: Transform.translate(
-          offset: Offset(animationTextPostitionLeftSideXAxis!.value, 0),
-          child: Text(
-            "Yalla Bena Now $index",
-            style: TextStyle(
-              fontSize: 25,
-              color: animateTextColor!.value,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+Route _createRoute(Widget destination) {
+  return PageRouteBuilder(
+    pageBuilder: (BuildContext context, Animation<double> animation,
+            Animation<double> secondaryAnimation) =>
+        destination,
+    transitionDuration: const Duration(milliseconds: 1200),
+    reverseTransitionDuration: const Duration(milliseconds: 1200),
+    transitionsBuilder: (BuildContext context, Animation<double> animation,
+        Animation<double> secondaryAnimation, Widget child) {
+      return FadeTransition(
+        opacity: CurvedAnimation(
+            parent: animation,
+            curve: Curves.easeOutCirc,
+            reverseCurve: Curves.easeOutCirc.flipped),
+        child: child,
+      );
+    },
+  );
 }
